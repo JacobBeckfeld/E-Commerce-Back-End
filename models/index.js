5458043f17ae369f13bf419b77debbb6de6d9944
JacobBeckfeld <jacbeck456@gmail.com>
2021-07-19 17:00:35 -0500

    filled in the model index.js file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_name",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_name",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+
+  as: "products"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+
+  as: "tags"
+})
 
 module.exports = {
   Product,
